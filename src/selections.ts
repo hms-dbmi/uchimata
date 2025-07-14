4d2bf51fe7ec8e8c2981ade01c19fc334cf135dc
David Kouril <david.kouril@gmail.com>
2025-07-14 13:54:45 -0400

    just adding placeholder

--- src/selections.ts
+++ src/selections.ts
@@ -1,6 +1,17 @@
 import type { Table } from "apache-arrow";
 
 /**
+ *
+ * Very simple cross-section of a model to test out filtering of the Arrow Table via duckdb
+ */
+function crosssection(model: Table, cutAt: number): Table {
+
+  console.log(`filtering model: cross-section at ${cutAt}`);
+
+  return model;
+}
+
+/**
  * Query for model parts on specified genomic coordinates
  * @param coordinates, e.g., "chr1" or "chr1:10000000-12000000" (chromosome annotation is linked to what's in ChromatinPart.label
  * @returns chromatin part, i.e., bins corresponding to the genomic coordinates
@@ -9,5 +20,7 @@ export function get(model: Table, coordinates: string): Table | null {
   console.warn("not implemented: get() for selections.ts");
   console.log(model);
   console.log(coordinates);
-  return null;
+
+  const cutAt = 0;
+  return crosssection(model, cutAt);
 }
